6ad2f1627d9bf32729e7dc2c327fc140f4b2c0df
lsjolinder <linus.sjolinder@b3.se>
2023-03-30 13:15:29 +0200

    Modified CaseManagement to be asynchronous in the creation of an errand in integrated systems.

---
+++ src/test/java/se/sundsvall/casemanagement/integration/opene/configuration/OpeneErrorDecoderTest.java
@@ -0,0 +1,96 @@
+package se.sundsvall.casemanagement.integration.opene.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFault;
+import javax.xml.soap.SOAPMessage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import se.sundsvall.dept44.exception.ClientProblem;
+
+import feign.Response;
+
+class OpeneErrorDecoderTests {
+    private OpeneErrorDecoder errorDecoder;
+    
+    
+    @BeforeEach
+    void setUp() {
+        errorDecoder = new OpeneErrorDecoder();
+    }
+    
+    
+    @Test
+    void shouldDecodeEmptyResponse() {
+        // given
+        Response response = mock(Response.class);
+        when(response.body()).thenReturn(null);
+        when(response.reason()).thenReturn("Bad Request");
+        
+        // when
+        Exception decodedError = errorDecoder.decode("someMethodKey", response);
+        
+        // then
+        assertThat(decodedError).isInstanceOf(ClientProblem.class);
+        assertThat(decodedError.getMessage()).isEqualTo("Bad Request: Bad request exception from OpenE Bad Request");
+    }
+    
+    @Test
+    void shouldHandleIOException() throws IOException {
+        // given
+        Response response = mock(Response.class);
+        Response.Body body = mock(Response.Body.class);
+        when(response.body()).thenReturn(body);
+        when(body.asInputStream()).thenThrow(new IOException("Failed to read response body"));
+        
+        // when
+        Exception decodedError = errorDecoder.decode("someMethodKey", response);
+        
+        // then
+        assertThat(decodedError).isInstanceOf(ClientProblem.class);
+        assertThat(decodedError.getMessage()).isEqualTo("Bad Request: Bad request exception from OpenE Failed to read response body");
+    }
+    
+    @Test
+    void shouldDecodeSOAPFault() throws Exception {
+        // given
+        Response response = mock(Response.class);
+        InputStream inputStream = new ByteArrayInputStream("<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><soap:Fault><faultcode>soap:Server</faultcode><faultstring>Internal Server Error</faultstring></soap:Fault></soap:Body></soap:Envelope>".getBytes());
+        Response.Body body = mock(Response.Body.class);
+        when(response.body()).thenReturn(body);
+        when(body.asInputStream()).thenReturn(inputStream);
+        
+        OpeneErrorDecoder decoder = new OpeneErrorDecoder() {
+            @Override
+            protected SOAPMessage createSOAPMessage(InputStream inputStream) throws SOAPException, IOException {
+                SOAPMessage message = mock(SOAPMessage.class);
+                SOAPBody soapBody = mock(SOAPBody.class);
+                SOAPFault soapFault = mock(SOAPFault.class);
+                when(soapBody.hasFault()).thenReturn(true);
+                when(soapBody.getFault()).thenReturn(soapFault);
+                when(soapFault.getFaultString()).thenReturn("Internal Server Error");
+                when(message.getSOAPBody()).thenReturn(soapBody);
+                return message;
+            }
+        };
+        
+        // when
+        Exception decodedError = decoder.decode("someMethodKey", response);
+        
+        // then
+        assertThat(decodedError).isInstanceOf(ClientProblem.class);
+        assertThat(decodedError.getMessage()).isEqualTo("Bad Request: Bad request exception from OpenE Internal Server Error");
+    }
+    
+    
+}
