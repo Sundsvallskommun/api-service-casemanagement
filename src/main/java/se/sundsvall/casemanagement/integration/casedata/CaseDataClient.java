3ddd854a7d5d05d94ded53ccf0c6f8b726157c98
Cheezï <linus.sjolinder@gmail.com>
2023-09-22 07:54:46 +0200

    Updated casedata integration

--- src/main/java/se/sundsvall/casemanagement/integration/casedata/CaseDataClient.java
+++ src/main/java/se/sundsvall/casemanagement/integration/casedata/CaseDataClient.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,35 +32,40 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @FeignClient(name = CaseDataConfiguration.REGISTRATION_ID, url = "${integration.case-data.url}", configuration = CaseDataConfiguration.class)
 @CircuitBreaker(name = CaseDataConfiguration.REGISTRATION_ID)
 public interface CaseDataClient {
-    
-    @PostMapping(path = "/errands", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PROBLEM_JSON_VALUE)
-    ResponseEntity<Void> postErrands(ErrandDTO errandDTO);
-    
-    @PatchMapping(path = "/errands/{id}/attachments", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PROBLEM_JSON_VALUE)
-    ResponseEntity<Void> patchErrandWithAttachment(@PathVariable Long id, AttachmentDTO attachmentDTO);
-    
-    
-    @GetMapping(path = "/errands/{id}", consumes = APPLICATION_JSON_VALUE, produces = {APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
-    ErrandDTO getErrand(@PathVariable Long id);
-    
-    @Operation(description = "Update errand.")
-    @PatchMapping(path = "/errands/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
-    @ApiResponse(responseCode = "204", description = "No content - Successful operation")
-    ResponseEntity<Void> patchErrand(@PathVariable Long id, @RequestBody @Valid PatchErrandDTO patchErrandDTO);
-    
-    @Operation(description = "Add/replace status on errand.")
-    @PutMapping(path = "/errands/{id}/statuses", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
-    @ApiResponse(responseCode = "204", description = "No content - Successful operation")
-    ResponseEntity<Void> putStatusOnErrand(@PathVariable Long id, @RequestBody @Valid List<StatusDTO> statusDTOList);
-    
-    @Operation(description = "Replace attachments on errand.")
-    @PutMapping(path = "/errands/{id}/attachments", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
-    @ApiResponse(responseCode = "204", description = "No content - Successful operation")
-    ResponseEntity<Void> putAttachmentsOnErrand(@PathVariable Long id, @RequestBody @Valid List<AttachmentDTO> attachmentDTOList);
-    
-    @Operation(description = "Replace stakeholders on errand.")
-    @PutMapping(path = "/errands/{id}/stakeholders", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
-    @ApiResponse(responseCode = "204", description = "No content - Successful operation")
-    ResponseEntity<Void> putStakeholdersOnErrand(@PathVariable Long id, @RequestBody @Valid List<StakeholderDTO> stakeholderDTOList);
-    
+
+	@PostMapping(path = "/errands", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PROBLEM_JSON_VALUE)
+	ResponseEntity<Void> postErrands(ErrandDTO errandDTO);
+
+	@PostMapping(path = "/attachments", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PROBLEM_JSON_VALUE)
+	ResponseEntity<Void> postAttachment(AttachmentDTO attachmentDTO);
+
+	@PutMapping(path = "/attachments/{attachmentId}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_PROBLEM_JSON_VALUE)
+	ResponseEntity<Void> putAttachment(@PathVariable Long attachmentId, AttachmentDTO attachmentDTO);
+
+	@Operation(description = "Get all attachments.")
+	@GetMapping(path = "/attachments/errand/{errand_number}", produces = {APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
+	List<AttachmentDTO> getAttachmentsByErrandNumber(@PathVariable String errand_number);
+
+	@Operation(description = "Delete attachment.")
+	@DeleteMapping(path = "/attachments/{id}", produces = APPLICATION_PROBLEM_JSON_VALUE)
+	ResponseEntity<Void> deleteAttachment(@PathVariable Long id);
+
+	@GetMapping(path = "/errands/{id}", consumes = APPLICATION_JSON_VALUE, produces = {APPLICATION_JSON_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
+	ErrandDTO getErrand(@PathVariable Long id);
+
+	@Operation(description = "Update errand.")
+	@PatchMapping(path = "/errands/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
+	@ApiResponse(responseCode = "204", description = "No content - Successful operation")
+	ResponseEntity<Void> patchErrand(@PathVariable Long id, @RequestBody @Valid PatchErrandDTO patchErrandDTO);
+
+	@Operation(description = "Add/replace status on errand.")
+	@PutMapping(path = "/errands/{id}/statuses", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
+	@ApiResponse(responseCode = "204", description = "No content - Successful operation")
+	ResponseEntity<Void> putStatusOnErrand(@PathVariable Long id, @RequestBody @Valid List<StatusDTO> statusDTOList);
+
+	@Operation(description = "Replace stakeholders on errand.")
+	@PutMapping(path = "/errands/{id}/stakeholders", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
+	@ApiResponse(responseCode = "204", description = "No content - Successful operation")
+	ResponseEntity<Void> putStakeholdersOnErrand(@PathVariable Long id, @RequestBody @Valid List<StakeholderDTO> stakeholderDTOList);
+
 }
