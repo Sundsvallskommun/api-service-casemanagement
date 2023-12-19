68c8f5630d0f4505e76652349c4942d0a216f109
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-12-19 11:32:46 +0100

    UF-6536: Switch to Java21

--- src/main/java/se/sundsvall/casemanagement/integration/db/model/CaseEntity.java
+++ src/main/java/se/sundsvall/casemanagement/integration/db/model/CaseEntity.java
@@ -2,15 +2,14 @@ package se.sundsvall.casemanagement.integration.db.model;
 
 import java.sql.Clob;
 
+import org.hibernate.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-
-import org.hibernate.Length;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,14 +23,15 @@ import lombok.With;
 @AllArgsConstructor
 public class CaseEntity {
 
-    @Id
-    private String id;
+	@Id
+	private String id;
 
-    @Lob
-    @Column(length = Length.LONG32)
-    private Clob dto;
+	@Lob
+	@Column(length = Length.LONG32)
+	private Clob dto;
 
-    @With
-    @Enumerated(EnumType.STRING)
-    private DeliveryStatus deliveryStatus;
+	@With
+	@Column(columnDefinition = "varchar(255)")
+	@Enumerated(EnumType.STRING)
+	private DeliveryStatus deliveryStatus;
 }
--- src/main/java/se/sundsvall/casemanagement/integration/db/model/CaseMapping.java
+++ src/main/java/se/sundsvall/casemanagement/integration/db/model/CaseMapping.java
@@ -1,13 +1,10 @@
 package se.sundsvall.casemanagement.integration.db.model;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-
-import se.sundsvall.casemanagement.api.model.enums.CaseType;
-import se.sundsvall.casemanagement.api.model.enums.SystemType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -17,10 +14,11 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.NotNull;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Objects;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import se.sundsvall.casemanagement.api.model.enums.CaseType;
+import se.sundsvall.casemanagement.api.model.enums.SystemType;
 
 @Entity(name = "CaseMapping")
 @IdClass(CaseMappingId.class)
@@ -29,49 +27,59 @@ import java.util.Objects;
 @SuperBuilder(setterPrefix = "with")
 public class CaseMapping {
 
-    @Id
-    @Column(unique = true, name = "externalCaseId")
-    private String externalCaseId;
-    @Id
-    @Column(name = "caseId")
-    private String caseId;
-    @NotNull
-    @Column(nullable = false, name = "system")
-    @Enumerated(EnumType.STRING)
-    private SystemType system;
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    @Column(name = "caseType")
-    private CaseType caseType;
-    @Column(name = "serviceName")
-    private String serviceName;
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    @Column(name = "timestamp")
-    private LocalDateTime timestamp;
+	@Id
+	@Column(unique = true, name = "externalCaseId")
+	private String externalCaseId;
 
-    public CaseMapping(String externalCaseId, String caseId, SystemType system, CaseType caseType, String serviceName) {
-        this.externalCaseId = externalCaseId;
-        this.caseId = caseId;
-        this.system = system;
-        this.caseType = caseType;
-        this.serviceName = serviceName;
-    }
+	@Id
+	@Column(name = "caseId")
+	private String caseId;
 
-    @PrePersist
-    @PreUpdate
-    protected void onPersistAndUpdate() {
-        timestamp = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
-    }
+	@NotNull
+	@Column(nullable = false, name = "system", columnDefinition = "varchar(255)")
+	@Enumerated(EnumType.STRING)
+	private SystemType system;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CaseMapping that)) return false;
-        return Objects.equals(externalCaseId, that.externalCaseId) && Objects.equals(caseId, that.caseId) && system == that.system && caseType == that.caseType && Objects.equals(serviceName, that.serviceName) && Objects.equals(timestamp, that.timestamp);
-    }
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(name = "caseType", columnDefinition = "varchar(255)")
+	private CaseType caseType;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(externalCaseId, caseId, system, caseType, serviceName, timestamp);
-    }
+	@Column(name = "serviceName")
+	private String serviceName;
+
+	@Schema(accessMode = Schema.AccessMode.READ_ONLY)
+	@Column(name = "timestamp")
+	private LocalDateTime timestamp;
+
+	public CaseMapping(String externalCaseId, String caseId, SystemType system, CaseType caseType, String serviceName) {
+		this.externalCaseId = externalCaseId;
+		this.caseId = caseId;
+		this.system = system;
+		this.caseType = caseType;
+		this.serviceName = serviceName;
+	}
+
+	@PrePersist
+	@PreUpdate
+	protected void onPersistAndUpdate() {
+		timestamp = LocalDateTime.now().truncatedTo(ChronoUnit.MICROS);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (!(o instanceof final CaseMapping that)) {
+			return false;
+		}
+		return Objects.equals(externalCaseId, that.externalCaseId) && Objects.equals(caseId, that.caseId) && (system == that.system) && (caseType == that.caseType) && Objects.equals(serviceName, that.serviceName) && Objects.equals(timestamp,
+			that.timestamp);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(externalCaseId, caseId, system, caseType, serviceName, timestamp);
+	}
 }
