81d3de3c12218bfad5f6acb5f583728b6f60c085
AndreSoderlund <andre.soderlund@b3.se>
2024-11-22 08:25:31 +0100

    Added party feignclient, updated casedata feignclient

--- src/main/java/se/sundsvall/casemanagement/integration/casedata/CaseDataClient.java
+++ src/main/java/se/sundsvall/casemanagement/integration/casedata/CaseDataClient.java
@@ -1,12 +1,16 @@
 package se.sundsvall.casemanagement.integration.casedata;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
-
-import java.util.List;
-
+import generated.client.casedata.Attachment;
+import generated.client.casedata.Errand;
+import generated.client.casedata.PatchErrand;
+import generated.client.casedata.Stakeholder;
+import generated.client.casedata.Status;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +21,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import se.sundsvall.casemanagement.integration.casedata.configuration.CaseDataConfiguration;
 
-import generated.client.casedata.Attachment;
-import generated.client.casedata.Errand;
-import generated.client.casedata.PatchErrand;
-import generated.client.casedata.Stakeholder;
-import generated.client.casedata.Status;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
 
 @FeignClient(name = CaseDataConfiguration.REGISTRATION_ID, url = "${integration.case-data.url}", configuration = CaseDataConfiguration.class)
 @CircuitBreaker(name = CaseDataConfiguration.REGISTRATION_ID)
@@ -111,5 +111,18 @@ public interface CaseDataClient {
 		@PathVariable(name = "namespace") final String namespace,
 		@PathVariable(name = "id") final Long id,
 		@RequestBody @Valid List<Stakeholder> stakeholderList);
+
+	@GetMapping(path = "/{municipalityId}/{namespace}/errands")
+	List<Errand> getErrands(
+		@PathVariable(name = "municipalityId") final String municipalityId,
+		@PathVariable(name = "namespace") final String namespace,
+		@RequestParam @Parameter(
+			description = "Syntax description: [spring-filter](https://github.com/turkraft/spring-filter/blob/85730f950a5f8623159cc0eb4d737555f9382bb7/README.md#syntax)",
+			example = "caseType:'PARKING_PERMIT' and stakeholders.firstName~'*mar*' and applicationReceived>'2022-09-08T12:18:03.747+02:00'",
+			schema = @Schema(implementation = String.class)) final String filter,
+		@RequestParam @Parameter(
+			description = "CaseData uses pagination but we always need to fetch every errand for the given filter. Set the size to a high number to fetch all errands.",
+			example = "1000",
+			schema = @Schema(implementation = String.class)) final String size);
 
 }
---
+++ src/main/java/se/sundsvall/casemanagement/integration/party/PartyClient.java
@@ -0,0 +1,25 @@
+package se.sundsvall.casemanagement.integration.party;
+
+import generated.client.party.PartyType;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import se.sundsvall.casemanagement.integration.party.configuration.PartyConfiguration;
+
+import java.util.Optional;
+
+import static se.sundsvall.casemanagement.integration.party.configuration.PartyConfiguration.CLIENT_ID;
+
+@FeignClient(name = CLIENT_ID,
+	url = "${integration.citizen.url}",
+	configuration = PartyConfiguration.class,
+	dismiss404 = true)
+public interface PartyClient {
+
+	@GetMapping("/{municipalityId}/{type}/{partyId}/legalId")
+	Optional<String> getLegalIdByPartyId(
+		@PathVariable String municipalityId,
+		@PathVariable PartyType type,
+		@PathVariable String partyId);
+
+}
