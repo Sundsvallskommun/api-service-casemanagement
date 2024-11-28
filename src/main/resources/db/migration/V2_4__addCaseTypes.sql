2c3b2d08be4afc4b5f8c8dcd40db3fd0593429e6
AndreSoderlund <andre.soderlund@b3.se>
2024-11-28 12:57:31 +0100

    added two new casetypedatas

---
+++ src/main/resources/db/migration/V2_4__addCaseTypes.sql
@@ -0,0 +1,3 @@
+INSERT INTO casetypedata
+VALUES ('ANSOKAN_RIVNINGSLOV', 'RIV', 'LOV', 'RL', 'ANSÖKAN', 'Rivningslov', 'Rivning', 'Rivningslov för rivning av'),
+       ('ANMALAN_RIVNING', 'D', 'LOV', 'ANM', 'ANM', 'Rivning', 'Riv', 'Anmälan för rivning av');
