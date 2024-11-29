af5a302799520427d28c971063a0f9a6ef807171
cheezi0747 <linus.sjolinder@b3.se>
2024-11-29 10:50:09 +0100

    UF-11177: Fix sonar issue

--- src/main/java/se/sundsvall/casemanagement/integration/alkt/AlkTService.java
+++ src/main/java/se/sundsvall/casemanagement/integration/alkt/AlkTService.java
@@ -2,22 +2,20 @@ package se.sundsvall.casemanagement.integration.alkt;
 
 import generated.client.alkt.ModelCase;
 import generated.client.alkt.Owner;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import se.sundsvall.casemanagement.api.model.CaseStatusDTO;
 import se.sundsvall.casemanagement.api.model.enums.SystemType;
 
-import java.util.List;
-
 @Service
 public class AlkTService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(AlkTService.class);
 	static final String CASE_TYPE = "ALKOHOLTOBAK";
 	static final String ONGOING = "Pågående";
 	static final String FINISHED = "Avslutad";
-
+	private static final Logger LOG = LoggerFactory.getLogger(AlkTService.class);
 	private final AlkTClient alkTClient;
 
 	public AlkTService(final AlkTClient alkTClient) {
@@ -25,18 +23,17 @@ public class AlkTService {
 	}
 
 	public List<CaseStatusDTO> getStatusesByPartyId(final String partyId, final String municipalityId) {
-		var owners = getOwnersByPartyId(partyId, municipalityId);
-		var modelCases = owners.stream()
+		final var owners = getOwnersByPartyId(partyId, municipalityId);
+		final var modelCases = owners.stream()
 			.flatMap(owner -> owner.getEstablishments().stream())
 			.flatMap(establishment -> establishment.getCases().stream())
 			.toList();
-		LOG.info("Found {} cases for partyId {}", modelCases.size(), partyId);
 		return mapToCaseStatuses(modelCases);
 	}
 
 	List<Owner> getOwnersByPartyId(final String partyId, final String municipalityId) {
 		LOG.info("Fetching owners for partyId: {} and municipalityId: {}", partyId, municipalityId);
-		var owners = alkTClient.getOwners(municipalityId, partyId);
+		final var owners = alkTClient.getOwners(municipalityId, partyId);
 		LOG.info("Found {} owners with partyId {}", owners.size(), partyId);
 		return owners;
 	}
