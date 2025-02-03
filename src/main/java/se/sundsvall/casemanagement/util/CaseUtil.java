e71008d279444bc1a7bced181fa106806ccb6640
André Söderlund <92517165+AndreSoderlund@users.noreply.github.com>
2025-02-03 11:23:50 +0100

    Revert "UF-10672 Added the new case types as attachment types (#165)"
    This reverts commit f72aadaceb3b89984233a1ac4ee9f76e53357921.

--- src/main/java/se/sundsvall/casemanagement/util/CaseUtil.java
+++ src/main/java/se/sundsvall/casemanagement/util/CaseUtil.java
@@ -25,6 +25,7 @@ public final class CaseUtil {
 	}
 
 	public static String getSokigoFormattedOrganizationNumber(final String organizationNumber) {
+
 		// Control that the organizationNumber is not null and that it is a valid length
 		if (IntStream.of(13, 12, 11, 10).anyMatch(i -> organizationNumber.length() == i)) {
 			// Remove all non-digit characters
